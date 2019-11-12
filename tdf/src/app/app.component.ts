2c527299aaef7d0478e45a12203389942454a1d8
alma996 <almasadikovic96@gmail.com>
2019-11-12 14:51:19 +0100

    Treca promjena

--- tdf/src/app/app.component.ts
+++ tdf/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +12,12 @@ export class AppComponent {
   topics = ['Angular', 'React', 'Vue'];
 
   topicHasError = true;
+  submitted = false;
+  errorMsg = '';
 
-  userModel = new User('Armin', 'armin@test.com', 555555, 'default', 'morning', true);
+  userModel = new User('Armin', 'armin@test.com', 5555555555, 'default', 'evening', true);
+
+  constructor(private _enrollmentService: EnrollmentService){}
 
   validateTopic(value){
     if(value === 'default'){
@@ -19,5 +25,16 @@ export class AppComponent {
     }else{
       this.topicHasError=false;
     }
+  }
+
+  onSubmit(userForm){
+    console.log(userForm);
+    /*this.submitted= true;
+    this._enrollmentService.enroll(this.userModel).subscribe(
+      data=> console.log('Success!', data),
+      error => this.errorMsg = error.statusText
+      );*/
+      
+    
   }
 }
